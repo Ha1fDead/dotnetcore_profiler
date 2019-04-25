611a5adbd9a135e3ebeb98acdb4a8fefbb8851b8
Nathan Lafferty <nslafferty@gmail.com>
2019-04-25 13:47:38 -0500

    added very simple time monitoring

--- middleware/contrastmiddleware.cs
+++ middleware/contrastmiddleware.cs
@@ -12,11 +12,29 @@ namespace middleware
             _requestDelegate = requestDelegate;
         }
 
+        /**
+        
+        Goals:
+
+        Measure the total time spent processing the request.
+        Measure the size of the response body in bytes. Calculate the minimum, average, and maximum responses seen so far.
+        Add new content to HTML pages to display information gathered by your project.
+        Ensure that your IHttpModule or Middleware is thread-safe and can correctly handle multiple concurrent requests.
+        Handle multiple encodings and different types of pages.
+        Use server resources (i.e. memory and processor) efficiently.
+        Include a small web application that demonstrates the behavior of your IHttpModule (or Middleware.) This web application is not the focus of the project and you should feel free to use the web application template projects provided by Visual Studio.
+
+         */
         public async Task Invoke(HttpContext context)
         {
-            Console.WriteLine("invoked");
-
+            // Request Delegates run in the order they are registered
+            // So we need to ensure ours runs first
+            DateTime start = DateTime.UtcNow;
             await _requestDelegate.Invoke(context);
+            DateTime end = DateTime.UtcNow;
+
+            Console.WriteLine((end - start).ToString());
+
         }
     }
 }
