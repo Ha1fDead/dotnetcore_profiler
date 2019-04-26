4740df0afb66dc66e740d3bdfe47b0c286f8a745
Nathan Lafferty <nslafferty@gmail.com>
2019-04-26 14:44:16 -0500

    improved stream behavior

--- middleware/HtmlInsertMiddleware.cs
+++ middleware/HtmlInsertMiddleware.cs
@@ -21,35 +21,41 @@ namespace middleware
         public async Task InvokeAsync(HttpContext context)
         {
             var capturedBody = context.Response.Body;
-            capturedBody.Dispose();
             
-            using (var updatedBody = new MemoryStream())
+            // What happens if someone has some content already written to the response?
+            // Pretty sure this shouldn't be possible given middleware architecture -- not supposed to do anything with the request until the way back out of the middleware
+            try
             {
-                context.Response.Body = updatedBody;
-                await _requestDelegate.Invoke(context);
-                context.Response.Body = capturedBody;
-
-                if (RequestDataCollectorMiddleware.RequestTimes.Any())
+                using (var swapBody = new MemoryStream())
                 {
-                    var shortest = RequestDataCollectorMiddleware.RequestTimes.Min((data) => {
-                        return data.RequestBodyLengthBytes;
-                    });
-                    var longest = RequestDataCollectorMiddleware.RequestTimes.Max((data) => {
-                        return data.RequestBodyLengthBytes;
-                    });
-                    var avgSize = RequestDataCollectorMiddleware.RequestTimes.Average((data) => {
-                        return data.RequestBodyLengthBytes;
-                    });
+                    context.Response.Body = swapBody;
+                    await _requestDelegate.Invoke(context);
 
-                    // This could be improved by intelligently detecting a "Drop Point" in the target HTML
-                    var length = Encoding.ASCII.GetBytes($"Min size: {shortest}, Average size: {avgSize}, Max size: {longest}.");
-                    await updatedBody.WriteAsync(length, 0, length.Length);
-                    context.Response.ContentLength = updatedBody.Length + length.Length;
+                    if (RequestDataCollectorMiddleware.RequestTimes.Any())
+                    {
+                        var shortest = RequestDataCollectorMiddleware.RequestTimes.Min((data) => {
+                            return data.RequestBodyLengthBytes;
+                        });
+                        var longest = RequestDataCollectorMiddleware.RequestTimes.Max((data) => {
+                            return data.RequestBodyLengthBytes;
+                        });
+                        var avgSize = RequestDataCollectorMiddleware.RequestTimes.Average((data) => {
+                            return data.RequestBodyLengthBytes;
+                        });
+
+                        // This could be improved by intelligently detecting a "Drop Point" in the target HTML
+                        var length = Encoding.ASCII.GetBytes($"Min size: {shortest}, Average size: {avgSize}, Max size: {longest}.");
+                        await swapBody.WriteAsync(length, 0, length.Length);
+                        context.Response.ContentLength = swapBody.Length + length.Length;
+                    }
+
+                    swapBody.Seek(0, SeekOrigin.Begin);
+                    await swapBody.CopyToAsync(capturedBody);
                 }
-
-                updatedBody.Seek(0, SeekOrigin.Begin);
-                var newContent = new StreamReader(updatedBody).ReadToEnd();
-                await context.Response.WriteAsync(newContent);
+            }
+            finally
+            {
+                context.Response.Body = capturedBody;
             }
 
         }
