48c91e0a4692b3783b5b29d7b3432a907e6f0dc3
Nathan Lafferty <nslafferty@gmail.com>
2019-04-28 10:14:58 -0500

    implemented profilerlogic tests

--- middleware.test/ProfilerLogic.cs
+++ middleware.test/ProfilerLogic.cs
@@ -1,20 +1,44 @@
 using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Text;
 using Xunit;
 
 namespace middleware.test
 {
-    public class ProfilerLogic
+    public class ProfilerLogicTest
     {        
-        [Fact(Skip = "Scaffold")]
+        [Fact]
         public void GetInjected_NothingToInject_Empty()
         {
-            Assert.True(false);
+            // Arrange
+            ProfilerLogic.ProfiledRequests = new ConcurrentBag<RequestProfiledModel>();
+
+            // Act
+            var res = ProfilerLogic.GetInjected();
+
+            // Assert
+            Assert.NotNull(res);
+            Assert.Empty(res);
         }
 
-        [Fact(Skip = "Scaffold")]
+        [Fact]
         public void GetInjected_ProperlyEncodesBinaryData()
         {
+            // Arrange
+            ProfilerLogic.ProfiledRequests = new ConcurrentBag<RequestProfiledModel>();
+            ProfilerLogic.ProfiledRequests.Add(new RequestProfiledModel(TimeSpan.FromDays(1), 1000)); // longest
+            ProfilerLogic.ProfiledRequests.Add(new RequestProfiledModel(TimeSpan.FromDays(1), 100)); 
+            ProfilerLogic.ProfiledRequests.Add(new RequestProfiledModel(TimeSpan.FromDays(1), 10)); // min
 
+            // Act
+            var res = ProfilerLogic.GetInjected();
+
+            // Assert
+            Assert.NotNull(res);
+            Assert.NotEmpty(res);
+            var message = Encoding.ASCII.GetString(res);
+            Assert.NotEmpty(message);
         }
     }
 }
