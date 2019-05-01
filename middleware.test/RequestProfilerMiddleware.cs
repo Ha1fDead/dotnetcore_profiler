c8baadac12dc7f8378902596bc7c9780f474b3c5
Nathan Lafferty <nslafferty@gmail.com>
2019-04-30 22:15:51 -0500

    implemented controller unit test for request profiler

--- middleware.test/RequestProfilerMiddleware.cs
+++ middleware.test/RequestProfilerMiddleware.cs
@@ -1,18 +1,39 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Xunit;
 
 namespace middleware.test
 {
     public class RequestDataCollectionMiddleware
     {
-        [Fact(Skip = "Scaffold")]
-        public void Invoke_RecordsRuntime()
+        [Fact()]
+        public async void Invoke_RecordsRuntime()
         {
             // Arrange
+            const string mockResponse = "This is a test";
+            ProfilerLogic.ProfiledRequests = new System.Collections.Concurrent.ConcurrentBag<RequestProfiledModel>();
+            var requestProfilerMiddleware = new RequestProfilerMiddleware(async (innerHttpContext) => {
+                await innerHttpContext.Response.WriteAsync(mockResponse);
+                innerHttpContext.Request.ContentLength = Encoding.ASCII.GetByteCount(mockResponse);
+            });
+
+            var contextStub = new DefaultHttpContext();
+            var bodyStub = new MemoryStream();
+            contextStub.Features.Get<IHttpResponseFeature>().Body = bodyStub;
 
             // Act
+            await requestProfilerMiddleware.InvokeAsync(contextStub);
 
             // Assert
+            Assert.NotEmpty(ProfilerLogic.ProfiledRequests);
+            Assert.Equal(1, ProfilerLogic.ProfiledRequests.Count);
+            var profiledResponse = ProfilerLogic.ProfiledRequests.First();
+            Assert.Equal(Encoding.ASCII.GetByteCount(mockResponse), profiledResponse.RequestBodyLengthBytes);
+            Assert.NotEqual(0, profiledResponse.RequestDuration.Ticks);
         }
 
         [Fact(Skip = "Scaffold")]
